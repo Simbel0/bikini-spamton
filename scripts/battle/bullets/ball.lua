f9f9396468a2a5cfe80c73ef3b40cbd5505251b0
Simbel <61120464+Simbel0@users.noreply.github.com>
2025-08-31 03:04:04 +0200

    fuck

--- scripts/battle/bullets/ball.lua
+++ scripts/battle/bullets/ball.lua
@@ -2,7 +2,7 @@ local SmallBullet, super = Class(Bullet)
 
 function SmallBullet:init(x, y, dir, speed)
     -- Last argument = sprite path
-    super.init(self, x, y, "bullets/ball"..math.random(1,3))
+    super.init(self, x, y, "bullets/ball"..love.math.random(1,3))
 
     -- Move the bullet in dir radians (0 = right, pi = left, clockwise rotation)
     self.physics.direction = dir
